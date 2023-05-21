e3be75d1b409a42420790b7091fa81c5de337566
Rafał Majewski <goodheropl@gmail.com>
2023-05-21 08:23:58 +0200

    Quiz question module

---
+++ src/features/quiz_questions/quiz_questions_service/QuizQuestionEntity.ts
@@ -0,0 +1,25 @@
+import {
+	Entity,
+	Column,
+	PrimaryGeneratedColumn,
+	ManyToOne,
+	JoinColumn,
+	type Relation,
+} from "typeorm";
+import QuizEntity from "../../quizzes/quizzes_service/QuizEntity.js";
+
+@Entity({name: "quiz_questions"})
+export default class QuizQuestionEntity {
+	@PrimaryGeneratedColumn("uuid", {name: "id"})
+	public readonly id!: string;
+
+	@Column({name: "quiz_id", type: "uuid"})
+	public readonly quizId!: string;
+
+	@Column({name: "content", type: "text"})
+	public readonly content!: string;
+
+	@ManyToOne(() => QuizEntity, (quiz) => quiz.quizQuestions)
+	@JoinColumn({referencedColumnName: "id", name: "quiz_id"})
+	public readonly quiz!: Relation<QuizEntity>;
+}
--- src/features/quizzes/quizzes_module/QuizzesModule.ts
+++ src/features/quizzes/quizzes_module/QuizzesModule.ts
@@ -3,9 +3,10 @@ import QuizzesController from "../quizzes_controller/QuizzesController.js";
 import QuizzesService from "../quizzes_service/QuizzesService.js";
 import QuizEntity from "../quizzes_service/QuizEntity.js";
 import {TypeOrmModule} from "@nestjs/typeorm";
+import QuizQuestionEntity from "../../quiz_questions/quiz_questions_service/QuizQuestionEntity.js";
 
 @Module({
-	imports: [TypeOrmModule.forFeature([QuizEntity])],
+	imports: [TypeOrmModule.forFeature([QuizEntity, QuizQuestionEntity])],
 	controllers: [QuizzesController],
 	providers: [QuizzesService],
 })
